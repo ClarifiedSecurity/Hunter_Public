b326690e779307617e659f52c745810e0a357822
hunter_01@WAYNE.CORP <hunter_01@WAYNE.CORP>
2018-04-10 17:12:50 +0300

    initial

---
+++ modules/Get-SanityTest-AnalyzeStack.ps1
@@ -0,0 +1,5 @@
+﻿if (-Not (Test-Path -Path "*sanitytest.csv")) {
+    return
+}
+Write-Host Running $(Split-Path $PSCommandPath -Leaf)
+& logparser /q:on "select count(*) as ct,user from *.csv group by user order by ct"
---
+++ modules/Get-SanityTest.ps1
@@ -0,0 +1,8 @@
+﻿$object = "" | Select-Object user
+if ($env:computername.endsWith("10")) {
+	$object.user = "chuck_norris"
+}
+else {
+	$object.user = $(whoami)
+}
+$object
